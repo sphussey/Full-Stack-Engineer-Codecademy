894d246dd6a108fcd56a831e449b2c82e5f88d2b
Shane <hussey.shane6@gmail.com>
2024-03-01 00:13:23 -0500

    Created schoolcatalogue.js

--- JavaScript/library.js
+++ JavaScript/library.js
@@ -35,7 +35,7 @@ class Media {
         this._isCheckedOut = status;
       }
     }
-  }
+}
   
   
 class Book extends Media {
@@ -53,7 +53,7 @@ class Book extends Media {
     get pages() {
       return this._pages;
     }
-  }
+}
   
   
 class Movie extends Media {
@@ -71,7 +71,7 @@ class Movie extends Media {
     get title() {
       return super.title;
     }
-  }
+}
   
   
 const historyOfEverything = new Book("Bill Bryson", "A Short History of Nearly Everything", 544);
